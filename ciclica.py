62ed5e6d795d05195d37a35db36f98316bfa43a0
crisen <crisen.cordova@gmail.com>
2020-04-06 22:39:17 -0500

    mi primer commit, revisar la condición en el if

---
+++ ciclica.py
@@ -0,0 +1,18 @@
+"""for i in range(0,5):
+	print("Iteración" + str(i))
+for i in range(4):
+	print("Iteración" + str(i))
+for i in "Mitocode":
+	#print (i)
+	print (i + "")"""
+
+contador = 34
+while contador < 5:
+	print(contador)
+	contador -= 1
+	if contador == -50:
+		print("Fin de Iteración")
+		break
+
+
+
